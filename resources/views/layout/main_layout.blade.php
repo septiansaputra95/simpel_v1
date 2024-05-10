1c2ab9ace2180d9dd32772219d2e52f48689513e
septiansaputra95m <septiansap@gmail.com>
2024-05-10 09:14:12 +0700

    perbaikan menu

--- resources/views/layout/main_layout.blade.php
+++ resources/views/layout/main_layout.blade.php
@@ -69,51 +69,29 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
                 <li><a href="{{ route('poliklinik.antrian.poli') }}" class="dropdown-item">Antrian Poli </a></li>
               </ul>
-            <li class="nav-item dropdown">
+            </li class="nav-item dropdown">
 
             <li class="nav-item dropdown">
               <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">BPJS</a>
               <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
                 <li><a href="{{ route('antrol.index') }}" class="dropdown-item">Antrol</a></li>
               </ul>
-            <li class="nav-item dropdown">
+            </li class="nav-item dropdown">
 
             <li class="nav-item">
               <a href="{{ route('suhuruangan.index') }}" class="nav-link">Suhu Ruangan</a>
             </li>
 
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Gudang Umum</a>
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-              <li><a href="{{ route('permintaan.index') }}" class="dropdown-item">Permintaan </a></li>
-              <li><a href="{{ route('permintaan.index') }}" class="dropdown-item">Master Barang</a></li>
-              <li><a href="{{ route('permintaan.laporan') }}" class="dropdown-item">Laporan Permintaan</a></li>
+            <li class="nav-item dropdown">
+              <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Gudang Umum</a>
+              <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                <li><a href="{{ route('permintaan.index') }}" class="dropdown-item">Permintaan </a></li>
+                <li><a href="{{ route('permintaan.index') }}" class="dropdown-item">Master Barang</a></li>
+                <li><a href="{{ route('permintaan.laporan') }}" class="dropdown-item">Laporan Permintaan</a></li>
+              </ul>
+            </li class="nav-item dropdown">
 
-              <li class="dropdown-divider"></li>
-
-              <!-- Level two dropdown-->
-              <li class="dropdown-submenu dropdown-hover">
-                <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Hover for action</a>
-                <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-                  <li>
-                    <a tabindex="-1" href="#" class="dropdown-item">level 2</a>
-                  </li>
-
-                  <!-- Level three dropdown-->
-                  <li class="dropdown-submenu">
-                    <a id="dropdownSubMenu3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">level 2</a>
-                    <ul aria-labelledby="dropdownSubMenu3" class="dropdown-menu border-0 shadow">
-                      <li><a href="#" class="dropdown-item">3rd level</a></li>
-                      <li><a href="#" class="dropdown-item">3rd level</a></li>
-                    </ul>
-                  </li>
-                  <!-- End Level three -->
-
-                  <li><a href="#" class="dropdown-item">level 2</a></li>
-                  <li><a href="#" class="dropdown-item">level 2</a></li>
-                </ul>
-              </li>
-              <!-- End Level two -->
-            </ul>
+            
           </li>
         </ul>
 
